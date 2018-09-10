35e2532e6e3eb5e39de3b886f4662665d41934e6
Rachel Pan <rpan06@gmail.com>
2018-09-10 16:09:29 -0700

    import export demo

--- app/index.js
+++ app/index.js
@@ -1,1 +1,7 @@
-console.log('App working: open app/index.js to start exploring ES6');
+import addToDom from './add_to_dom'
+//import {add,subtract,multiply,divide} from './math'
+import * as math from './math'
+
+console.log('Math: ', math)
+
+addToDom('h1', `52-10 = ${math.subtract(52,10)}`);
